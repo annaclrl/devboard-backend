537e0c4df701080117ce2e60a7cfd426f89d1b28
Anna <annaclararussoluca@gmail.com>
2025-12-26 00:45:46 -0300

    feat: criando entidade usuario

---
+++ src/main/java/com/devboard/model/Usuario.java
@@ -0,0 +1,33 @@
+package com.devboard.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "db_usuario", uniqueConstraints = {
+        @UniqueConstraint(name = "uk_db_usuario_email", columnNames = "email")
+})
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "db_usuario_seq")
+    @SequenceGenerator(name = "db_usuario_seq", sequenceName = "db_usuario_seq", allocationSize = 1)
+    @Column(name = "id_usuario")
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String nome;
+
+    @Column(nullable = false, length = 250)
+    private String email;
+
+    @Column(nullable = false, length = 50)
+    private String senha;
+}
