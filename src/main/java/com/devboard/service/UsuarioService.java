d53edbc6a22fff978973d67cbce076fbe0198547
Anna <annaclararussoluca@gmail.com>
2025-12-29 21:33:21 -0300

    feat: criando método de criar usuario na service

--- src/main/java/com/devboard/service/UsuarioService.java
+++ src/main/java/com/devboard/service/UsuarioService.java
@@ -1,4 +1,25 @@
 package com.devboard.service;
 
+import com.devboard.dto.usuario.UsuarioRequestDTO;
+import com.devboard.dto.usuario.UsuarioResponseDTO;
+import com.devboard.mapper.UsuarioMapper;
+import com.devboard.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public UsuarioResponseDTO salvarUsuario(UsuarioRequestDTO dto) {
+        var usuarioEntity = UsuarioMapper.toEntity(dto);
+        var usuarioSalvo = usuarioRepository.save(usuarioEntity);
+        var usuarioResponse = UsuarioMapper.toResponseDTO(usuarioSalvo);
+        return usuarioResponse;
+    }
+
 }
